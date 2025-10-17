174163ee33faf950ade3f5a6a357d83c3b4900d9
rosendo <rosendc30@gmail.com>
2025-10-16 22:45:00 -0300

    adicionando items a lista

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,18 @@
-const addInput = document.getElementById('add-input');
+const addInput = document.querySelector('.add-input');
 const form = document.getElementById('add-item');
-
-
-
-const items = ["Pão de forma","Café preto","Suco de laranja","Bolacha"];
-
-
-
+const list = document.getElementById('items');
+const button = document.querySelector('#btn');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 })
-addInput.addEventListener('input', () => {
-    const value = addInput.value.trim();
-    console.log(value)
-})
-function renderItems() {
 
+button.addEventListener('click', addItems)
+
+function addItems() {
+    const item = `<li class="item"><div><input type="checkbox"><span>`+addInput.value.trim()+`</span></div><a href="#"><img src="../assets/trash.svg"></a></li>`;
+    
+    list.insertAdjacentHTML('afterbegin', item);
+    addInput.value = '';
+   
 }

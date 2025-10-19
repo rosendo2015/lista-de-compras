ec9e37e8214c979a42a9bd0641011eadb3f5f29c
rosendo <rosendc30@gmail.com>
2025-10-19 12:33:47 -0300

    deletar items

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,58 @@
-const addInput = document.querySelector('.add-input');
-const form = document.getElementById('add-item');
-const list = document.getElementById('items');
-const button = document.querySelector('#btn');
+form = document.getElementById("form");
+input = document.getElementById("add-item");
+ul = document.getElementById("items");
 
-const items = document.querySelectorAll('.item');
+let listCompras = [
+    {id: 1, produto: 'Pão de forma'},
+    {id: 2, produto: 'Café preto'},
+    {id: 3, produto: 'Suco de laranja'},
+    {id: 4, produto: 'Bolacha'}]
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
+form.addEventListener("submit", function(event){
+    event.preventDefault()
 })
 
-button.addEventListener('click', addItems)
-
-function addItems() {
-    if (addInput.value.trim() === '') {
-        alert('Favor digitar um item válido.');
-        return;
-    }else {
-    const item = `<li class="item"><div><input type="checkbox"><span>`+addInput.value.trim()+`</span></div><a href="#"><img src="../assets/trash.svg"></a></li>`;
-    
-    list.insertAdjacentHTML('afterbegin', item);
-    addInput.value = '';
+function addItemList(){
+    let = itemObj = {
+        id: listCompras.length + 1,
+        produto: input.value
     }
+    listCompras.unshift(itemObj) 
+    input.value = ""    
+    mostrarLista()   
+}
+function mostrarLista(){
+    let html = "";
+    listCompras.map(item =>{
+        return(
+
+            html += `
+            <li>
+            <div>
+                <input type="checkbox">${item.produto}
+            </div>
+            <div>
+            <button onclick="editarItem(${item.id})">
+                <img src="assets/pencil.svg" alt="Editar">
+            </button>
+            <button onclick="deletarItem(${item.id})">
+                <img src="assets/trash.svg" alt="lixeira">
+            </button>
+            </div>
+            </li>
+            `
+        )
+    })
+    ul.innerHTML = html;
+}
+function editarItem(id){
+    
+    item(id);
+}
+function deletarItem(id){
+    let novaLista = listCompras.filter(item => item.id !== id);
+    console.log(novaLista);
+    listCompras = novaLista;
+
+    mostrarLista();
 }
